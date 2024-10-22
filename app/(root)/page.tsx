0639b32ae0e47a9ca1a0b472a09739ac37858a79
Denitsa Slavkova <deni9309@gmail.com>
2024-10-22 22:16:36 +0300

    Integrate clerk authentication

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,13 @@
+import { UserButton } from '@clerk/nextjs'
+
 const Home = () => {
-  return <div className="text-2xl text-purple-500">PixelMind</div>
+  return (
+    <div className="text-2xl text-purple-500">
+      <p>Home</p>
+
+      <UserButton afterSignOutUrl='/' />
+    </div>
+  )
 }
 
 export default Home
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { IBM_Plex_Sans } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
 
 import './globals.css'
 import { cn } from '@/lib/utils'
@@ -21,11 +22,13 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
-      <head>
-        <link rel="icon" href="/favicon.svg" />
-      </head>
-      <body className={cn('font-IBMPlex antialiased', IBMPlex.variable)}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <head>
+          <link rel="icon" href="/favicon.svg" />
+        </head>
+        <body className={cn('font-IBMPlex antialiased', IBMPlex.variable)}>{children}</body>
+      </html>
+    </ClerkProvider>
   )
 }
