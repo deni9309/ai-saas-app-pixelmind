09f31e8113e3e3678f226ddab4d0fee07b961d31
Denitsa Slavkova <deni9309@gmail.com>
2024-10-23 22:16:14 +0300

    Implement sidebar component

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
+import Sidebar from '@/components/shared/sidebar'
+
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="root">
+      <Sidebar /> 
+
+      {/* <MobileNav /> */}
+
       <div className="root-container">
         <div className="wrapper">{children}</div>
       </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider appearance={{ variables: { colorPrimary: '#624cf5' } }}>
       <html lang="en">
         <head>
           <link rel="icon" href="/favicon.svg" />
